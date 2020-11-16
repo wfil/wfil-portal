477ae321755b36305823c398c8cfba74ca23eafd
Cristiam Da Silva <cristiam86@gmail.com>
2020-11-16 18:15:21 +0100

    added codefi partner

--- src/components/HomePartners/HomePartners.js
+++ src/components/HomePartners/HomePartners.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import codefi from './codefi.png'
 import uniswap from './uniswap.png'
 import protocol from './protocol-labs.png'
 import filecoin from './filecoin.png'
@@ -7,30 +8,32 @@ import consensysLabs from './consensys-labs.png'
 import textile from './textile.png'
 import './HomePartners.scss';
 
+const PARTNERS = [
+  [
+    { img: consensysLabs, url: 'https://labs.mesh.xyz/tachyon/', style: { padding: '30px' }  },
+    { img: protocol, url: 'https://protocol.ai', style: { flex: '0 0 22%' }},
+    { img: filecoin, url: 'https://filecoin.io', style: { flex: '0 0 8%' }},
+  ], 
+  [
+    { img: textile, url: 'https://textile.io' },
+    { img: codefi, url: 'https://codefi.consensys.net' },
+  ]
+]
+
 const HomePartners = () => {
   return (
     <div className="home-partners">
       <h3>Partners</h3>
       <div className="home-partners-logos">
-        <div className="home-partners-logos-row">
-          <div className="home-partners-logos-item" style={{ padding: '30px' }}>
-            <img src={consensysLabs}/>
+        {PARTNERS.map(partnersRow => (
+          <div className="home-partners-logos-row">
+            {partnersRow.map(({img, url, style = {}}) => (
+              <div className="home-partners-logos-item" style={style}>
+                <a href={url} target="_blank" rel="noopener noreferrer"><img src={img}/></a>
+              </div>
+            ))}
           </div>
-          <div className="home-partners-logos-item" style={{ flex: '0 0 22%' }}>
-            <img src={protocol}/>
-          </div>
-          <div className="home-partners-logos-item logo-auto">
-            <img src={filecoin}/>
-          </div>
-          <div className="home-partners-logos-item">
-            <img src={textile}/>
-          </div>
-        </div>
-        {/* <div className="home-partners-logos-row">
-          <div className="home-partners-logos-item">
-            <img src={uniswap}/>
-          </div> 
-        </div>*/}
+        ))}
       </div>
     </div>
   )
