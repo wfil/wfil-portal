6fac7ded7eeca0195f8e34dab9f4bc112b1023eb
Cristiam Da Silva <cristiam86@gmail.com>
2020-11-24 00:16:03 +0100

    Added Tachyon logo

--- src/components/HomePartners/HomePartners.js
+++ src/components/HomePartners/HomePartners.js
@@ -4,17 +4,17 @@ import codefi from './codefi.png'
 import uniswap from './uniswap.png'
 import protocol from './protocol-labs.png'
 import filecoin from './filecoin.png'
-import consensysLabs from './consensys-labs.png'
+import tachyon from './tachyon.png'
 import textile from './textile.png'
 import './HomePartners.scss';
 
 const PARTNERS = [
   [
-    // { img: consensysLabs, url: 'https://labs.mesh.xyz/tachyon/', style: { padding: '30px' }  },
+    { img: tachyon, url: 'https://labs.mesh.xyz/tachyon/' },
     { img: protocol, url: 'https://protocol.ai', style: { flex: '0 0 22%' }},
-    { img: filecoin, url: 'https://filecoin.io', style: { flex: '0 0 8%' }},
   ], 
   [
+    { img: filecoin, url: 'https://filecoin.io', style: { flex: '0 0 8%' }},
     { img: textile, url: 'https://textile.io' },
     // { img: codefi, url: 'https://codefi.consensys.net' },
   ]
