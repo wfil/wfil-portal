dde21de945070c8446b94b2c64b756eb0a3fdbb0
Cristiam Da Silva <cristiam86@gmail.com>
2020-12-28 02:16:39 +0100

    removed tuemilio subscription widget

--- src/components/RequestAccess/RequestAccess.js
+++ src/components/RequestAccess/RequestAccess.js
@@ -1,60 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import isEmail from 'validator/lib/isEmail';
-
 import Button from '../Button';
 
 import './RequestAccess.scss';
 
 const RequestAccess = () => {
-  const [waitingEmail, setWatingEmail] = useState('');
-  const [formError, setFormError] = useState(false);
-  const [formSuccess, setFormSuccess] = useState(false);
-
-  useEffect(() => {
-    const script = document.createElement("script");
-
-    script.src = "/tuemilo_script.js";
-    script.async = true;
-
-    document.body.appendChild(script);
-  }, [])
-
-  const sendVisit = () => {
-    setFormError(false);
-
-    if (!isEmail(waitingEmail)) {
-      setFormError(true);
-      return;
-    }
-    if (window && window.Tuemilio) {
-      console.log("RequestAccess -> window.Tuemilio", window.Tuemilio)
-      window.Tuemilio('createSubscriber', { address: waitingEmail });
-      setFormSuccess(true);
-    }
-  }
 
   return (
     <div className="request-access-form">
-      {formSuccess 
-        ? (<p>Thank you! follow our path to mainnet launch in our community channels</p>)
-        : (
-          <>
-            <p>Subscribe for news! 🚀</p>
-            <div className="request-access-form-input">
-              <input
-                className={formError ? 'has-error' : ''}
-                type="email"
-                value={waitingEmail}
-                onChange={({target: { value }}) => setWatingEmail(value)}
-                placeholder="Enter your email"
-              />
-            </div>
-            <Button onClick={sendVisit}>Subscribe</Button>
-          </>
-        )
-      }
-      
-      <div id="t-signup"></div>
+      <Button href="https://wfil.substack.com/welcome" alias="link" target="_blank" rel="noopener noreferrer">
+        Subscribe for news! 🚀
+      </Button>
     </div>
   )
 }
